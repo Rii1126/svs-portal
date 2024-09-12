ec5476152cad674ad11a0b215dc22b8f50921397
0xyuri <96197787+Rii1126@users.noreply.github.com>
2024-09-13 02:32:41 +0800

    update

--- src/api/apiUtility.js
+++ src/api/apiUtility.js
@@ -1,61 +1,22 @@
-import axios from "axios";
-import { apiConfig } from "./apiConfig";
-import { getTokenFromCookie } from "../utils/cookieUtils";
+// apiUtility.js
 
-// Create an Axios instance
-const apiClient = axios.create({
-  baseURL: apiConfig.baseURL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: false, // Only necessary if the backend requires credentials (cookies, etc.)
-});
-
-// Add a request interceptor to dynamically add the Authorization header
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = getTokenFromCookie(); // Get the token from cookie or another storage
-    if (token) {
-      config.headers["Authorization"] = `${token}`; // Set the Bearer token dynamically
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Handle API requests
-export const apiRequest = async (
-  method,
-  endpoint,
-  data = null,
-  params = null
-) => {
+export const apiRequest = async (url, options = {}) => {
   try {
-    const response = await apiClient({
-      method,
-      url: endpoint,
-      data,
-      params, // Optional query parameters
+    const response = await fetch(url, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
     });
-    return response.data; // Return the response data
-  } catch (error) {
-    console.error(
-      `API Error: ${error.response?.status} ${error.response?.statusText}`
-    );
-    if (error.response) {
-      // Return a structured error object
-      return {
-        success: false,
-        status: error.response.status,
-        message: error.response.data.message || "An error occurred",
-      };
-    } else {
-      return {
-        success: false,
-        message: "User not found!",
-      };
+
+    if (!response.ok) {
+      throw new Error("API Request failed");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("API Request Error:", error);
+    throw error;
   }
 };
